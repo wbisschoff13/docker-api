770c356d8eb413d231f09ae3a0b611d791b359bd
Werner Bisschoff <wbisschoff13@gmail.com>
2021-02-17 14:16:20 +0200

    added 0.0.0.0 to supported hosts, added chmod +x wait-for

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*@=_e12p-*8996a5qnk=zb%47uy20iy%@t!!w2hp#_l0-*$s_e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
